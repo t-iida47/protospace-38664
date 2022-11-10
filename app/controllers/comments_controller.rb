f673092fea93db25b1ba1d9b8b65d48daf5ce063
t-iida47 <g.sil.tiida18@gmail.com>
2022-11-10 15:01:27 +0900

    修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,13 +6,14 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:content, :prototype, :user).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 
 end
